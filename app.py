42bc2716692292dfc1ee5f2b7e758176c6b7663c
Ethan1817 <ethan.chauvin81@gmail.com>
2026-08-15 18:23:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-'''import streamlit as st
+import streamlit as st
 import pandas as pd
 import json
 import os
@@ -20,8 +20,4 @@ def charger_catalogue():
     ]
 
 PRODUITS_EXACTS = charger_catalogue()
-'''
-import streamlit as st
 
-st.title("🎉 Test réussi !")
-st.write("L'application fonctionne correctement.")
